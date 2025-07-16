5bfee5c083428e7bb683f2e18b158f1a6840701b
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-07-16 15:42:12 +0200

    feat(faq visitors): visitors page now requires a selection of pdf to display FAQs

--- src/components/home/visitors-faq-page.jsx
+++ src/components/home/visitors-faq-page.jsx
@@ -3,14 +3,32 @@ import "./visitors-faq-page.css";
 import axios from "axios";
 
 export default function FaqVisitorPage() {
-  const [faqs, setfaqs] = useState([]);
+  const [pdfs, setPdfs] = useState([]);
+  const [faqs, setFaqs] = useState([]);
   const apiUrl = import.meta.env.VITE_API_URL;
 
   function fetchPdfs() {
     axios
-      .get(`${apiUrl}/faqs/`)
+      .get(`${apiUrl}/uploadedfiles/`)
       .then((res) => {
-        setfaqs(res.data);
+        console.log(res.data);
+        setPdfs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function fetchFaqs(pdf_id) {
+    axios
+      .get(`${apiUrl}/faqs/`, {
+        params: {
+          file_id: pdf_id,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setFaqs(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -20,9 +38,25 @@ export default function FaqVisitorPage() {
   useEffect(() => {
     fetchPdfs();
   }, []);
+
   return (
     <div className="faq-visitor-page">
-      <h1>FAQs</h1>
+      <h1>Choose a file to view FAQs</h1>
+      <div className="pdf-list-container">
+        <select
+          className="pdf-list"
+          onChange={(e) => {
+            fetchFaqs(e.target.value);
+          }}
+        >
+          <option selected disabled>Select a file</option>
+          {pdfs.map((pdf) => (
+            <option key={pdf.id} value={pdf.id}>
+              {pdf.file_name}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="faq-list">
         <h2>Questions & Answers</h2>
         {faqs.map((faq, index) => (
